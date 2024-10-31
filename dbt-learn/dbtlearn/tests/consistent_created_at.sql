5f589d2fad28d7039e14f24908ee78e98faa44c4
Qaladid <abdiaziz11410@gmail.com>
2024-11-01 00:14:53 +0300

    my first commit

---
+++ dbt-learn/dbtlearn/tests/consistent_created_at.sql
@@ -0,0 +1,14 @@
+-- tests/consistent_created_at.sql
+
+SELECT 
+    r.review_date,
+    l.created_at
+FROM 
+    {{ ref('fct_reviews') }} r
+JOIN 
+    {{ ref('dim_listings_cleansed') }} l
+ON 
+    r.listing_id = l.listing_id
+WHERE 
+    r.review_date < l.created_at
+
